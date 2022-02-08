574156c1b6dc19e04655e48b789931b93a510c10
papgergo1997 <pap.gergo.1997@gmail.com>
2022-02-08 15:10:23 +0100

    firebase added tp project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSelectModule } from '@angular/material/select';
 import { ItemEditComponent } from './pages/admin/item-list/item-edit/item-edit.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+
+
 
 @NgModule({
   declarations: [AppComponent, ItemListComponent, ItemEditComponent],
@@ -33,7 +41,12 @@ import { ItemEditComponent } from './pages/admin/item-list/item-edit/item-edit.c
     MatIconModule,
     MatDialogModule,
     MatSelectModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'flyingwhale-625ae',
+    appId: '1:685032215704:web:0d9bda1ef8bd066472aced',
+    databaseURL: 'https://flyingwhale-625ae-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'flyingwhale-625ae.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCYzbVQlIT75E_uUgSXKa7qUFZafIxcEdI',
+    authDomain: 'flyingwhale-625ae.firebaseapp.com',
+    messagingSenderId: '685032215704',
+    measurementId: 'G-NF6CZ9LV5Q',
+  },
   production: true
 };
