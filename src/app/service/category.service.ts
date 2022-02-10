0663c0dae6b6c3f8f61b61968bc644eddeec584d
papgergo1997 <pap.gergo.1997@gmail.com>
2022-02-10 12:46:24 +0100

    category service created

---
+++ src/app/service/category.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Category } from '../model/category';
+import { BaseService } from './base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService extends BaseService<Category>  {
+  constructor(public http: HttpClient) {
+    super(http, 'categorys');
+   }
+}
--- src/app/service/item.service.ts
+++ src/app/service/item.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
 import { Item } from '../model/item';
-import { map } from 'rxjs/operators';
 import { BaseService } from './base.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ItemService extends BaseService<Item>{
+export class ItemService extends BaseService<Item> {
   constructor(public http: HttpClient) {
-    super(http, 'items')
+    super(http, 'items');
   }
 }
