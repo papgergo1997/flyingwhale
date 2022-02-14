9b360ddbfa105f41ffe32477f2f5956e10eac317
papgergo1997 <pap.gergo.1997@gmail.com>
2022-02-14 18:25:55 +0100

    user token added to post requests

--- src/app/service/base.service.ts
+++ src/app/service/base.service.ts
@@ -10,6 +10,7 @@ import { AuthService } from './auth.service';
 export class BaseService<T extends { id: string }> {
   list$: BehaviorSubject<T[]> = new BehaviorSubject<T[]>([]);
   URL: string = '';
+  currentUser: any;
   baseURL: string =
     'https://flyingwhale-625ae-default-rtdb.europe-west1.firebasedatabase.app';
 
@@ -19,29 +20,56 @@ export class BaseService<T extends { id: string }> {
     @Inject('entityName') entityName: string
   ) {
     this.URL = `${this.baseURL}/${entityName}`;
+    this.currentUser = JSON.parse(localStorage.getItem('user'));
     this.getAll();
   }
 
   getAll(): void {
-    this.authService.currentUser.pipe(
-      take(1),
-      exhaustMap((user) => {
-        return this.http.get(`${this.URL}.json?auth=${user.token}`);
-      }),
-      map((resp) => {
-        const arr = [];
-        for (const key in resp) {
-          if (resp.hasOwnProperty(key)) {
-            arr.push({ ...resp[key], id: key });
+    this.http
+      .get(`${this.URL}.json?auth=${this.currentUser._token}`)
+      .pipe(
+        map((resp) => {
+          const arr = [];
+          for (const key in resp) {
+            if (resp.hasOwnProperty(key)) {
+              arr.push({ ...resp[key], id: key });
+            }
           }
-        }
-        return arr;
-      })
-    ).subscribe(list=>this.list$.next(list));
+          return arr;
+        })
+      )
+      .subscribe((list) => this.list$.next(list));
+    // this.authService.currentUser
+    //   .pipe(
+    //     take(1),
+    //     exhaustMap((user) => {
+    //       return this.http.get(`${this.URL}.json?auth=${user.token}`);
+    //     }),
+    //     map((resp) => {
+    //       const arr = [];
+    //       for (const key in resp) {
+    //         if (resp.hasOwnProperty(key)) {
+    //           arr.push({ ...resp[key], id: key });
+    //         }
+    //       }
+    //       return arr;
+    //     })
+    //   )
+    //   .subscribe((list) => this.list$.next(list));
   }
 
   create(doc: T): void {
-    this.http.post<T>(`${this.URL}.json`, doc).subscribe(() => this.getAll());
+    this.http
+      .post<T>(`${this.URL}.json?auth=${this.currentUser._token}`, doc)
+      .subscribe(() => this.getAll());
+    // this.authService.currentUser
+    //   .pipe(
+    //     take(1),
+    //     exhaustMap((user) => {
+    //       return this.http.post<T>(`${this.URL}.json?auth=${user.token}`, doc);
+    //     })
+    //   )
+    //
   }
 
   update(doc: T): void {
