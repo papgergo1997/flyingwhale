81b033db4f67fced4b0e6b611fdf714dada5b2bb
papgergo1997 <pap.gergo.1997@gmail.com>
2022-02-15 16:52:13 +0100

    category edit created

--- src/app/pages/admin/category-list/category-edit/category-edit.component.ts
+++ src/app/pages/admin/category-list/category-edit/category-edit.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CategoryService } from 'src/app/service/category.service';
 
 @Component({
   selector: 'app-category-edit',
   templateUrl: './category-edit.component.html',
-  styleUrls: ['./category-edit.component.scss']
+  styleUrls: ['./category-edit.component.scss'],
 })
 export class CategoryEditComponent implements OnInit {
+  form = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl(''),
+    description: new FormControl(''),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private catService: CategoryService,
+    private dialogRef: MatDialogRef<CategoryEditComponent>,
+    @Inject(MAT_DIALOG_DATA) data: any
+  ) {
+    this.form.patchValue(data);
   }
 
+  ngOnInit(): void {
+    this.catService.getAll();
+  }
+
+  onSubmit(): void {
+    if (this.form.get('id').value == '') {
+      this.catService.create(this.form.value);
+      this.dialogRef.close();
+    } else {
+      this.catService.update(this.form.value);
+      this.dialogRef.close();
+    }
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
 }
