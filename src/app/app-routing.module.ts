a6ce3594dd7ec84ddf6e4dbd2093a422d4a438ed
papgergo1997 <pap.gergo.1997@gmail.com>
2022-02-19 15:55:10 +0100

    about page basics created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { CategoryListComponent } from './pages/admin/category-list/category-list
 import { HomeComponent } from './pages/main/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AuthGuardService, LoginGuardService } from './service/auth-guard.service';
+import { AboutComponent } from './pages/main/about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'login', component: LoginComponent, canActivate: [LoginGuardService]},
   {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
   { path: 'items', component: ItemListComponent, canActivate: [AuthGuardService] },
   { path: 'categorys', component: CategoryListComponent, canActivate: [AuthGuardService] },
 ];
